0ae8a50282095416bd76fe6d7c06fe419b5eb7eb
teamursamajor <teamursamajor@gmail.com>
2023-01-14 12:09:17 -0500

    put back auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -59,7 +59,7 @@ public class RobotContainer {
    *
    * @return the command to run in autonomous
    */
-  public Command getGyroCommand() {
+  public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return new GyroCommand(new GyroSubsystem());
   }
