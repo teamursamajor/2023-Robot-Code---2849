b3d502c7ebc31af2ab04b5c7f5cd0468729c9234
teamursamajor <teamursamajor@gmail.com>
2023-01-26 16:26:52 -0500

    align

---
+++ src/main/java/frc/robot/commands/ConeDrop.java
@@ -0,0 +1,87 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimeLightSubsystem;
+
+public class ConeDrop extends CommandBase {
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    double armLength;  //horizontal length of arm when arm is fully extended
+    double xValue;  //limelght deteced x value
+    double centerX;  //x value of where reflective tape needs to be in order to be aligned with arm
+    double range;  //decied on a range
+    double distance; // used methods in the subsystem to gt the distance
+    double ofSetAngle; // ty
+    boolean isHigh; 
+    boolean xAligned;
+    boolean alignFinished;
+    private final DriveSubsystem DRIVE_SUBSYSTEM;
+    private final LimeLightSubsystem LIME_LIGHT; 
+
+    public ConeDrop(DriveSubsystem driveSubsystem, LimeLightSubsystem limeLightSubsystem, boolean isHigh){
+        this.isHigh = isHigh;  //true is going for high pole, flase if going for mid
+        DRIVE_SUBSYSTEM = driveSubsystem;
+        LIME_LIGHT = limeLightSubsystem;
+        addRequirements(DRIVE_SUBSYSTEM);
+        addRequirements(LIME_LIGHT);
+    }
+
+    @Override
+    public void initialize() {
+        //stop robot, turn off controls.
+        alignFinished = false;
+        xAligned = false;
+    }
+
+    @Override
+    public void execute() {
+        if(isHigh){
+            distance = LIME_LIGHT.getDistanceHigh();
+        }else{
+            distance = LIME_LIGHT.getDistanceMid();
+        }
+        double x = LIME_LIGHT.getX();
+
+        if(!xAligned){
+            if (x == Double.MIN_VALUE) {
+                System.out.println("Couldn't detect limelight");
+                return;
+            } else if (x <= (centerX+range) && x >= (centerX-range)) {
+                // stop robot set speed 0
+                xAligned = true;
+            } else if (x > centerX+range) {
+                //move left
+            } else if (x < centerX-range) {
+                //move right
+            }
+        }else{
+            if(distance <= armLength + range && distance >= armLength - range){
+                alignFinished  =true;
+            }else if(distance>armLength+range){
+                //go foward
+            }else if(distance<armLength-range){
+                //go backward
+            }
+        }
+
+    
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+       //stop robot
+       //drop cone
+    }
+
+
+    @Override
+    public boolean isFinished() {
+
+        return alignFinished;
+    }
+
+
+
+    
+
+}
---
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -0,0 +1,72 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LimeLightSubsystem extends SubsystemBase {
+    double heightMid = 56.0; // the height of the mid pole in cm
+    double heightHigh = 106; // the height of the high pole in cm
+    double heightOfCam = 30; //the hieght of the limelight camera to the ground, CHNAGE
+    double camAngle = 15.0; // angles of limelight camera, CHNAGE
+    
+
+
+    //give the horizontal distance from robot to the mid pole
+    //angleOffSet is f=given by limelight (ty)
+    public double getDistanceMid(){
+        //placeholder so code is not mad
+        //h2 is heightMid
+        //a2 is angleOffSet
+        //h1 is heightOfCam
+        //a1 is camAngle
+        //d = (h2-h1) / tan(a1+a2)   formula
+        double totAngle = camAngle+getOffsetAngle();
+        double totAngleToRadian = totAngle * (3.14159 / 180.0);
+        return (heightMid-heightOfCam)/(Math.tan(totAngleToRadian));
+    }
+
+    public double getDistanceHigh () {
+        //h2 is heightHigh
+        //a2 is angleOffSet
+        //h1 is heightOfCam
+        //a1 is camAngle
+        //d = (h2-h1) / tan(a1+a2)   formula
+        double totAngle = camAngle+getOffsetAngle();
+        double totAngleToRadian = totAngle * (3.14159 / 180.0);
+        return (heightMid-heightOfCam)/(Math.tan(totAngleToRadian));
+    }
+
+
+
+
+      public double getX() {
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry tx = table.getEntry("tx");
+        NetworkTableEntry tv = table.getEntry("tv");
+        double x;
+
+        double isThereLimelight = tv.getDouble(0.0);
+        if (isThereLimelight == 0) {
+            x = Double.MIN_VALUE;
+        } else {
+            x = tx.getDouble(Double.MIN_VALUE);
+        }
+    
+        SmartDashboard.putNumber("LimelightX", x);
+        return x;
+  }
+
+  private double getOffsetAngle(){
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+    NetworkTableEntry ty = table.getEntry("ty");
+    double targetOffsetAngle_Vertical = ty.getDouble(Double.MIN_VALUE);
+    return targetOffsetAngle_Vertical;
+  }
+
+
+
+    
+}
